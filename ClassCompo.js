d60c2cf20e6630a4f433c235ef632e8081c53526
ErShubham2908 <shubhamprepbytes007@gmail.com>
2023-03-21 22:20:04 +0530

    first

---
+++ ClassCompo.js
@@ -0,0 +1,15 @@
+import { Component } from "react"
+
+class ClassCompo extends Component{
+    render(){
+        return (
+            <>
+                <h1>This is created using Class Components</h1>
+                <p>This is Done by using External CSS</p>
+                <p>This is Done by using Inline CSS</p>
+            </>
+        )
+    }
+}
+
+export default ClassCompo;
---
+++ Jsa.js
@@ -0,0 +1,5 @@
+let count = false;
+
+function setCount(){
+    console.log(!count);
+}
